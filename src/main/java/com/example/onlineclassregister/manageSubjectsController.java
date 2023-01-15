a69bb9a67b12dda2a8a8ec41667b571ae017822f
rcalinescu@valeo-it.com <rcalinescu@valeo-it.com>
2023-01-15 13:09:39 +0200

    Courses Done

--- src/main/java/com/example/onlineclassregister/manageSubjectsController.java
+++ src/main/java/com/example/onlineclassregister/manageSubjectsController.java
@@ -1,5 +1,6 @@
 package com.example.onlineclassregister;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -53,6 +54,70 @@ public class manageSubjectsController {
 
 
     private final ObservableList<Student> dataList = FXCollections.observableArrayList();
+
+
+    private void filterSubjects(String filter) {
+        List<Subject> subjects = Subject.initSubjectList();
+
+        // Create new lists to hold filtered data
+        ObservableList<Integer> ids = FXCollections.observableArrayList();
+        ObservableList<String> names = FXCollections.observableArrayList();
+        ObservableList<String> teachers = FXCollections.observableArrayList();
+        ObservableList<Double> averages = FXCollections.observableArrayList();
+        ObservableList<Button> edits = FXCollections.observableArrayList();
+
+        for (Subject s : subjects) {
+            if (s.name.toLowerCase().contains(filter.toLowerCase())) {
+                ids.add(s.id);
+                names.add(s.name);
+
+                String teacherList = "";
+                for (Teacher t : Teacher.getTeachers()) {
+                    if (t.subjectId == s.id) {
+                        teacherList += t.fName + " " + t.lName + ", ";
+                    }
+                }
+
+                if (!teacherList.isEmpty()) {
+                    teacherList = teacherList.substring(0, teacherList.length() - 2);
+                }
+
+                teachers.add(teacherList);
+                averages.add(s.getAverage());
+
+                if (s != null) {
+                    Button b = new Button();
+                    b.setText("Edit");
+                    b.setUserData(s);
+                    b.setStyle("-fx-background-color: #66bbc4; -fx-border-radius: 25px; -fx-background-radius: 25px");
+                    b.setPrefWidth(44);
+                    b.setPrefHeight(23);
+                    b.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent event) {
+                            try {
+                                editButtonClick(b);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            } catch (SQLException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    });
+                    edits.add(b);
+                }
+            }
+        }
+
+        // Platform.runLater()
+        Platform.runLater(() -> {
+            id.setItems(ids);
+            name.setItems(names);
+            teacher.setItems(teachers);
+            average.setItems(averages);
+            edit.setItems(edits);
+        });
+    }
 
     @FXML
     private void initialize() throws SQLException {
@@ -111,6 +176,15 @@ public class manageSubjectsController {
                 });
                 edits.add(b);
             }
+
+            ///Thread for filtering
+            filterField.textProperty().addListener((observable, oldValue, newValue) -> {
+                Thread thread = new Thread(() -> {
+                    filterSubjects(newValue);
+                });
+                thread.start();
+            });
+
         }
 
         id.setItems(ids);
@@ -119,32 +193,8 @@ public class manageSubjectsController {
         average.setItems(averages);
         edit.setItems(edits);
 
-        filterField.textProperty().addListener((observable, oldValue, newValue) -> {
-            // When the search string changes, update the list to only show items that match
-            ObservableList<Integer> filteredIds = FXCollections.observableArrayList();
-            ObservableList<String> filteredNames = FXCollections.observableArrayList();
-            ObservableList<String> filteredTeachers = FXCollections.observableArrayList();
-            ObservableList<Double> filteredAverages = FXCollections.observableArrayList();
-            ObservableList<Button> filteredEdits = FXCollections.observableArrayList();
 
-            for (int i = 0; i < ids.size(); i++) {
-                String subjectName = names.get(i);
-                if (subjectName.toLowerCase().contains(newValue.toLowerCase())) {
-                    filteredIds.add(ids.get(i));
-                    filteredNames.add(subjectName);
-                    filteredTeachers.add(teachers.get(i));
-                    filteredAverages.add(averages.get(i));
-                    filteredEdits.add(edits.get(i));
-                }
-            }
-
-            id.setItems(filteredIds);
-            name.setItems(filteredNames);
-            teacher.setItems(teachers);
-            average.setItems(averages);
-            edit.setItems(filteredEdits);
-        });
-    }
+        }
 
     private void editButtonClick(Button b) throws IOException, SQLException {
         Subject s = (Subject) b.getUserData();
@@ -161,57 +211,7 @@ public class manageSubjectsController {
 
         stage.showAndWait();
 
-        ObservableList<Integer> ids = FXCollections.observableArrayList();
-        ObservableList<String> names = FXCollections.observableArrayList();
-        ObservableList<String> teachers = FXCollections.observableArrayList();
-        ObservableList<Double> averages = FXCollections.observableArrayList();
-        ObservableList<Button> edits = FXCollections.observableArrayList();
-
-// update the list of subjects, clear the current data in the lists and repopulate the lists with the updated data
-        List<Subject> subjects = Subject.initSubjectList();
-
-        for (Subject sb : subjects) {
-            ids.add(sb.id);
-            names.add(sb.name);
-
-            for(int i = 0; i < Teacher.getTeachers().size(); i++) {
-                Teacher t = Teacher.getTeachers().get(i);
-                if(t.subjectId==sb.id) {
-                    if(i == Teacher.getTeachers().size() - 1) {
-                        teachers.add("Finish");
-                    } else {
-                        teachers.add(t.fName+ " "+ t.lName+", ");
-                    }
-                }
-            }
-            averages.add(sb.getAverage());
-
-            if(sb!=null) {
-                Button bt = new Button();
-                bt.setText("Edit");
-                bt.setUserData(sb);
-                bt.setStyle("-fx-background-color: #66bbc4; -fx-border-radius: 25px; -fx-background-radius: 25px");
-                bt.setPrefWidth(44);
-                bt.setPrefHeight(23);
-                bt.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        try {
-                            editButtonClick(bt);
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        } catch (SQLException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                });
-                edits.add(bt);
-            }
-        }
-
-
-        name.setItems(names);
-        edit.setItems(edits);
+        initialize();
 
     }
 
@@ -268,7 +268,7 @@ public class manageSubjectsController {
     public void addSubjectClick() throws IOException, SQLException {
 
 
-        loggedUser.fromCsv=0;
+        loggedUser.fromCsv = 0;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("addSubject.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         Stage stage = new Stage();
@@ -276,59 +276,8 @@ public class manageSubjectsController {
         stage.setScene(scene);
         stage.showAndWait();
 
-        ObservableList<Integer> ids = FXCollections.observableArrayList();
-        ObservableList<String> names = FXCollections.observableArrayList();
-        ObservableList<String> teachers = FXCollections.observableArrayList();
-        ObservableList<Double> averages = FXCollections.observableArrayList();
-        ObservableList<Button> edits = FXCollections.observableArrayList();
+        initialize();
 
-// update the list of subjects, clear the current data in the lists and repopulate the lists with the updated data
-        List<Subject> subjects = Subject.initSubjectList();
-
-        for (Subject sb : subjects) {
-            ids.add(sb.id);
-            names.add(sb.name);
-
-            for(int i = 0; i < Teacher.getTeachers().size(); i++) {
-                Teacher t = Teacher.getTeachers().get(i);
-                if(t.subjectId==sb.id) {
-                    if(i == Teacher.getTeachers().size() - 1) {
-                        teachers.add("Finish");
-                    } else {
-                        teachers.add(t.fName+ " "+ t.lName+", ");
-                    }
-                }
-            }
-            averages.add(sb.getAverage());
-
-            if(sb!=null) {
-                Button bt = new Button();
-                bt.setText("Edit");
-                bt.setUserData(sb);
-                bt.setStyle("-fx-background-color: #66bbc4; -fx-border-radius: 25px; -fx-background-radius: 25px");
-                bt.setPrefWidth(44);
-                bt.setPrefHeight(23);
-                bt.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        try {
-                            editButtonClick(bt);
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        } catch (SQLException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                });
-                edits.add(bt);
-            }
-        }
-
-        id.setItems(ids);
-        name.setItems(names);
-        teacher.setItems(teachers);
-        edit.setItems(edits);
-
-        }
+    }
 
 }
